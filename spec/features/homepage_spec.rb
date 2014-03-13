0113d4f387bba3e3137e6b3929f7557ffcdf16bc
Paul Stone <pmstone247@gmail.com>
2014-03-13 11:24:49 -0600

    Allow copyright date to change automatically

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe 'Visiting the home page' do
-  
+
   it "displays all of the menu items" do
     visit "/"
     expect(page).to have_content("Channa Masala")
     expect(page).to have_content("Chicken Tikka Masala")
   end
-  
+
+  it "displays the current year for the copyright" do
+    visit "/"
+
+    within("footer") do
+      expect(page).to have_content("2014")
+    end
+  end
 end
