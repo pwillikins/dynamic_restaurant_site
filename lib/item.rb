a78ab4141ae56384a65bd733647efd3625537e3b
Paul Stone <pmstone247@gmail.com>
2014-03-13 14:37:46 -0600

    made modifications and updates
    created item_spec and item.rb files
    created models folder under spec

---
+++ lib/item.rb
@@ -0,0 +1,19 @@
+class Item
+  def initialize(name, price, description)
+    @name = name
+    @price = price
+    @description = description
+  end
+
+  def name
+    @name
+  end
+
+  def price
+    @price
+  end
+
+  def description
+    @description
+  end
+end
